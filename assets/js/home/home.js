bed123ef6517d9bbd7bdabfacf43ec544f513384
fadilakarimli <fadilafk@code.edu.az>
2025-03-19 01:37:40 +0400

    search write js

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -46,3 +46,71 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+const searchIcon = document.querySelector('.search .fa-magnifying-glass');
+const searchOverlay = document.createElement('div');
+searchOverlay.classList.add('search-overlay');
+
+searchOverlay.innerHTML = `
+    <div class="search-box">
+        <input type="text" placeholder="Type your search...">
+    </div>
+`;
+
+const closeIcon = document.createElement('div');
+closeIcon.classList.add('close-icon');
+closeIcon.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
+
+searchOverlay.appendChild(closeIcon);
+
+document.body.appendChild(searchOverlay);
+
+searchIcon.addEventListener('click', () => {
+    searchOverlay.classList.add('active'); 
+});
+
+closeIcon.addEventListener('click', () => {
+    searchOverlay.classList.remove('active'); 
+});
+
+
+
+const menuBar = document.querySelector('.menu-bar i'); 
+const body = document.body;
+
+const sidebar = document.createElement('div');
+sidebar.classList.add('sidebar');
+
+const closeBtn = document.createElement('div');
+closeBtn.classList.add('close-menu');
+closeBtn.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
+
+const socialIcons = document.createElement('div');
+socialIcons.classList.add('social-icons');
+
+const socialMedia = [
+    { href: '#', className: 'fa fa-facebook' },
+    { href: '#', className: 'fa fa-twitter' },
+    { href: '#', className: 'fa fa-instagram' },
+    { href: '#', className: 'fa fa-linkedin' },
+];
+
+socialMedia.forEach(media => {
+    const link = document.createElement('a');
+    link.href = media.href;
+    link.innerHTML = `<i class="${media.className}"></i>`;
+    socialIcons.appendChild(link);
+});
+
+sidebar.appendChild(closeBtn);
+sidebar.appendChild(socialIcons);
+
+body.appendChild(sidebar);
+
+menuBar.addEventListener('click', () => {
+    sidebar.classList.add('active'); 
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('active'); 
+});
